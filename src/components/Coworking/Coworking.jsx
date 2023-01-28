ea073f33f5ffb40867efb1b8833de1146454271e
gabdykalyk <gabdykalyk@gmail.com>
2023-01-29 03:57:01 +0600

    feat: changed logoes

--- src/components/Coworking/Coworking.jsx
+++ src/components/Coworking/Coworking.jsx
@@ -1,13 +1,13 @@
 import CoworkingItem from "./CoworkingItem/CoworkingItem";
 import styles from "./Coworking.module.scss";
-import clock from "../../images/clock.svg";
-import guarantee from "../../images/guarantee.svg";
-import complect from "../../images/complect.svg";
 import quality from "../../images/quality.svg";
 import temp from "../../images/temp.svg";
 import design from "../../images/design.svg";
 import auto from "../../images/auto.svg";
-import set from "../../images/set.svg";
+import fast from "../../images/fast.svg";
+import calendar from "../../images/calendar.svg";
+import basket from "../../images/basket.svg";
+import complect from "../../images/complect.svg";
 import { AnimationOnScroll } from 'react-animation-on-scroll';
 
 const Coworking = () => {
@@ -18,13 +18,13 @@ const Coworking = () => {
             </div>
             <AnimationOnScroll animateIn="animate__fadeInLeft" animateOnce>
                 <div className={styles.coworkingWrapper}>
-                    <CoworkingItem src={clock} alt={'clock'} text={"Быстрые сроки исполнения заказов - от 14 дней"} />
-                    <CoworkingItem src={guarantee} alt={'guarantee'} text={"Мы даем гарантию на наши изделия на срок 180 дней"} />
+                    <CoworkingItem src={fast} alt={'clock'} text={"Быстрые сроки исполнения заказов - от 14 дней"} />
+                    <CoworkingItem src={calendar} alt={'guarantee'} text={"Мы даем гарантию на наши изделия на срок 180 дней"} />
                 </div>
             </AnimationOnScroll>
             <AnimationOnScroll animateIn="animate__fadeInRight" animateOnce>
                 <div className={styles.coworkingWrapper}>
-                    <CoworkingItem src={complect} alt={'clock'} text={"Большой ассортимент предлагаемых изделий"} />
+                    <CoworkingItem src={basket} alt={'clock'} text={"Большой ассортимент предлагаемых изделий"} />
                     <CoworkingItem src={quality} alt={'guarantee'} text={"Отличное соотношение цены и качества"} />
                 </div>
             </AnimationOnScroll>
@@ -37,7 +37,7 @@ const Coworking = () => {
             <AnimationOnScroll animateIn="animate__fadeInRight" animateOnce>
                 <div className={styles.coworkingWrapper}>
                     <CoworkingItem src={auto} alt={'auto'} text={"Наши изделия идеально подходят под автоматические линии розлива и запайки продукции"} />
-                    <CoworkingItem src={set} alt={'set'} text={"По запросу, мы укомплектуем стаканы крышкой и платинкой"} />
+                    <CoworkingItem src={complect} alt={'set'} text={"По запросу, мы укомплектуем стаканы крышкой и платинкой"} />
                 </div>
             </AnimationOnScroll>
         </div>
